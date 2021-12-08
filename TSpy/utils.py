755383b747da6c516735d3fbcd228a278ad7c51a
ChengYu <chengyu@nudt.edu.cn>
2021-12-08 18:21:18 +0800

    create utils modele.

---
+++ TSpy/utils.py
@@ -0,0 +1,14 @@
+# Created by Chengyu on 2021/12/8.
+# TSpy util collection.
+
+import numpy as np
+import math
+
+def normalize(data_tensor):
+    mean = np.mean(data_tensor)
+    var = np.var(data_tensor)
+    i = 0
+    for channel in data_tensor[0]:
+        data_tensor[0][i] = (channel - mean)/math.sqrt(var)
+        i += 1
+    return data_tensor
