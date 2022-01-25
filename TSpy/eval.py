67029bc540b92c5099d183c519bb819f18f5d91e
Chengyu <chengyu@nudt.edu.cn>
2022-01-26 07:13:36 +0800

    fix bugs

--- TSpy/eval.py
+++ TSpy/eval.py
@@ -1,31 +1,54 @@
 from sklearn import metrics
 import numpy as np
-from TSpy.label import adjust_label
+from TSpy.label import reorder_label
 
-def macro_precision(prediction, groundtruth):
+def evaluation(groundtruth, prediction):
+    pass
+
+def macro_recall(groundtruth, prediction, if_reorder_label=False):
     '''
     calculate macro precision
     '''
-    prediction = np.array(prediction)
-    groundtruth = np.array(groundtruth)
+    groundtruth = np.array(groundtruth, dtype=int)
+    prediction = np.array(prediction, dtype=int)
     if len(prediction) != len(groundtruth):
         print('prediction and groundtruth must be of the same length')
     else:
-        return metrics.precision_score(adjust_label(prediction),adjust_label(groundtruth),average='macro', zero_division=0)
+        if if_reorder_label:
+            groundtruth = reorder_label(groundtruth)
+            prediction = reorder_label(prediction)
+        return metrics.recall_score(groundtruth, prediction, average='macro', zero_division=0)
 
-def macro_f1score(prediction, groundtruth):
+def macro_precision(groundtruth, prediction, if_reorder_label=False):
+    '''
+    calculate macro precision
+    '''
+    groundtruth = np.array(groundtruth, dtype=int)
+    prediction = np.array(prediction, dtype=int)
+    if len(prediction) != len(groundtruth):
+        print('prediction and groundtruth must be of the same length')
+    else:
+        if if_reorder_label:
+            groundtruth = reorder_label(groundtruth)
+            prediction = reorder_label(prediction)
+        return metrics.precision_score(groundtruth, prediction, average='macro', zero_division=0)
+
+def macro_f1score(groundtruth, prediction, if_reorder_label=False):
     '''
     calculate macro f1 score
     '''
-    prediction = np.array(prediction)
-    groundtruth = np.array(groundtruth)
+    groundtruth = np.array(groundtruth, dtype=int)
+    prediction = np.array(prediction, dtype=int)
     if len(prediction) != len(groundtruth):
         print('prediction and groundtruth must be of the same length')
     else:
-        return metrics.f1_score(adjust_label(prediction),adjust_label(groundtruth),average='macro', zero_division=0)
+        if if_reorder_label:
+            groundtruth = reorder_label(groundtruth)
+            prediction = reorder_label(prediction)
+        return metrics.f1_score(groundtruth, prediction, average='macro', zero_division=0)
 
 def ARI(prediction, groundtruth):
-    return metrics.adjusted_rand_score(prediction, groundtruth)
+    return metrics.adjusted_rand_score(groundtruth, prediction)
 
 def AMI(prediction, groundtruth):
-    return metrics.adjusted_mutual_info_score(prediction, groundtruth)
+    return metrics.adjusted_mutual_info_score(groundtruth, prediction)
