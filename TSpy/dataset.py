92aa73940224e55e6a1375cfbb43cb5a121397dc
Chengyu <chengyu@nudt.edu.cn>
2022-02-04 12:46:31 +0800

    add dataset package

---
+++ TSpy/dataset.py
@@ -0,0 +1,20 @@
+import os
+from TSpy.label import seg_to_label
+import scipy.io
+import numpy as np
+
+def load_USC_HAD(subject, target, data_path):
+    prefix = os.path.join(data_path,'USC-HAD/Subject'+str(subject)+'/')
+    fname_prefix = 'a'
+    fname_postfix = 't'+str(target)+'.mat'
+    data_list = []
+    label_json = {}
+    total_length = 0
+    for i in range(1,13):
+        data = scipy.io.loadmat(prefix+fname_prefix+str(i)+fname_postfix)
+        data = data['sensor_readings']
+        data_list.append(data)
+        total_length += len(data)
+        label_json[total_length]=i
+    label = seg_to_label(label_json)
+    return np.vstack(data_list), label
