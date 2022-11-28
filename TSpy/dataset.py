f099e445d66461a5d3697566d635381b2752add6
ChengYu <chengyu@nudt.edu.cn>
2022-11-28 21:09:59 +0800

    add UEA dataset

--- TSpy/dataset.py
+++ TSpy/dataset.py
@@ -2,6 +2,18 @@ import os
 from TSpy.label import seg_to_label
 import scipy.io
 import numpy as np
+import json
+
+def load_UEA(dataset, path):
+    path = os.path.join(path, dataset+'/'+dataset)
+    train_data = np.load(path+'_TRAIN.npy')
+    test_data = np.load(path+'_TEST.npy')
+    train_labels = np.load(path+'_TRAIN_labels.npy')
+    test_labels = np.load(path+'_TEST_labels.npy')
+    with open(path+'_map.json') as f:
+        label_map = json.load(f)
+        f.close()
+    return train_data, train_labels, test_data, test_labels, label_map
 
 def load_USC_HAD(subject, target, dataset_path):
     prefix = os.path.join(dataset_path,'USC-HAD/Subject'+str(subject)+'/')
