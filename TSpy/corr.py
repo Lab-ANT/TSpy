0200fe3c17352e1c3162806ff0701d39dab7ad91
shanzhao <shanzhao@nudt.edu.cn>
2023-03-06 22:53:22 +0800

    update lagged state correlation

--- TSpy/corr.py
+++ TSpy/corr.py
@@ -54,15 +54,32 @@ def match_label(X, Y):
         Y[np.argwhere(Y==pair[1])] = pair[0]+5
     return Y
 
-def lagged_NMI(seq1, seq2, ratio):
-    atom_step = 0.01
+# def lagged_NMI(seq1, seq2, ratio):
+#     atom_step = 0.01
+#     length = len(seq1)
+#     k = int(ratio/atom_step)
+#     max_score = -1
+#     lag = 0
+#     for i in range(k):
+#         lag_len = int(k*atom_step*length)
+#         seq1 = np.concatenate([np.zeros(lag_len).flatten(), seq1[lag_len:].flatten()])
+#         NMI_score = metrics.normalized_mutual_info_score(seq1,seq2)
+#         if NMI_score >= max_score:
+#             max_score = NMI_score
+#             lag = lag_len
+#     return max_score, lag
+
+def lagged_NMI(seq1, seq2, ratio, atom_step=0.01):
     length = len(seq1)
     k = int(ratio/atom_step)
     max_score = -1
     lag = 0
-    for i in range(k):
+    for i in range(-k, k):
         lag_len = int(k*atom_step*length)
-        seq1 = np.concatenate([np.zeros(lag_len).flatten(), seq1[lag_len:].flatten()])
+        if lag_len>=0:
+            seq1 = np.concatenate([np.zeros(lag_len).flatten(), seq1[lag_len:].flatten()])
+        else:
+            seq1 = np.concatenate([seq1[:-lag_len].flatten(), np.zeros(-lag_len).flatten()])
         NMI_score = metrics.normalized_mutual_info_score(seq1,seq2)
         if NMI_score >= max_score:
             max_score = NMI_score
@@ -70,17 +87,29 @@ def lagged_NMI(seq1, seq2, ratio):
     return max_score, lag
 
 def lagged_state_correlation(seq_list, ratio=0.05):
+    '''
+    Lagged state correlation.
+    @Params:
+        seq_list: list of state sequences.
+        ratio: maximum lag ratio.
+    @return:
+        correlation_matrix: state correlation matrix.
+        lag_matrix: lag matrix.
+    '''
     num_instance = len(seq_list)
     correlation_matrix = np.ones((num_instance,num_instance))
+    lag_matrix = np.ones((num_instance, num_instance))
     for i in range(num_instance):
         for j in range(num_instance):
             if i < j:
-                NMI_score,_ = lagged_NMI(seq_list[i],seq_list[j], ratio)
+                NMI_score, lag = lagged_NMI(seq_list[i],seq_list[j], ratio)
                 correlation_matrix[i,j] = NMI_score
                 correlation_matrix[j,i] = NMI_score
+                lag_matrix[i,j] = lag
+                lag_matrix[j,i] = -lag
             else:
                 continue
-    return correlation_matrix
+    return correlation_matrix, lag_matrix
 
 def state_correlation(seq_list):
     length = len(seq_list)
